a55eaecb4bfb4aacb8ce8b736834720121e5f7e0
Olena <7890572@ukr.net>
2022-12-30 17:26:30 +0100

    Added code

--- remove.js
+++ remove.js
@@ -1,11 +1,11 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
-function arrEl(newArr, item) {
-    let item = array.indexOf(5);
-    while (item > -1) {
+function arrayItem(array, item) {
+    item = array.indexOf(5);
+    if(item > -1) {
         array.splice(item, 1);
     }
-return item;    
+    return array;    
 }
-console.log(arrEl(array));
 
+console.log(arrayItem(array));
